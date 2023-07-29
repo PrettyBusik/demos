43084488991f76020756218eb788732af6e14603
Viktor Pikaev <vikp@boozt.com>
2023-07-29 21:10:00 +0200

    Weather history: Add backend and example markup

---
+++ src/weather-history/get-current.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+$env = parse_ini_file(__DIR__ . '/.env');
+$url = sprintf(
+    "https://api.weatherapi.com/v1/current.json?key=%s&q=%s&days=3&aqi=no&alerts=no",
+    $env['WEATHERAPI_KEY'],
+    $_GET['place']
+);
+
+$response = file_get_contents($url);
+if ($response === false) {
+    header("HTTP/1.0 404 Not Found");
+} else {
+    header("HTTP/1.0 200 OK");
+    echo $response;
+}
