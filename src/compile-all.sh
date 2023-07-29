74386fa154c705aa1fa4f63a2ce06cd98888564c
Viktor Pikaev <vikp@boozt.com>
2023-07-29 18:11:00 +0200

    Add a bash script for compiling all frontend applications

---
+++ src/compile-all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+current_directory=$(dirname "$0")
+directories=()
+directories+=("./weather-forecast")
+
+# Prepare backend
+
+rm -r -f "$current_directory/runtime"
+mkdir -m 777 "$current_directory/runtime"
+
+# Build frontend applications
+export NG_CLI_ANALYTICS=ci
+
+for sub_directory in "${directories[@]}"; do
+  echo "----------------------------------------------------------------------------------------------------"
+  echo "$sub_directory"
+  echo "----------------------------------------------------------------------------------------------------"
+    cd "$current_directory/$sub_directory"
+    npm install
+    npm run build-prod
+    rm -r .angular
+    rm -r node_modules
+done
--- src/weather-forecast/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$env = parse_ini_file(__DIR__ . '/.env');
-header("HTTP/1.0 200 OK");
-echo json_encode(['mapsApiKey' => $env['YANDEX_MAP_API_KEY']]);
