3175fdffee7e8503dff2495f48f9f18e5b56b7ff
Marina Pikaeva <marycy.99@gmail.com>
2023-10-12 11:19:13 +0200

    add tests for history service

--- src/vocabulary-cards/src/app/services/history.service.spec.ts
+++ src/vocabulary-cards/src/app/services/history.service.spec.ts
@@ -36,20 +36,85 @@ describe('HistoryService', () => {
 
     expect(storage.set).toHaveBeenCalledWith("learned", expectation);
   });
-    it('getDatesWithTraining() - should get dates for trained words', function () {
-      let storage = jasmine.createSpyObj<StorageService>(['get']);
-      let history= new HistoryService(storage);
+  it('getDatesWithTraining() - should get dates for trained words', () => {
+    let storage = jasmine.createSpyObj<StorageService>(['get']);
+    let history = new HistoryService(storage);
 
-      let  days= new Set<number>();
-      days.add(111);
-      days.add(222);
-      days.add(333);
+    let days = new Set<number>();
+    days.add(111);
+    days.add(222);
+    days.add(333);
 
-      let prediction= storage.get.and.returnValue(days);
+    storage.get.and.returnValue(days);
 
-     let result= history.getDatesWithTraining();
+    let result = history.getDatesWithTraining();
 
-      expect(storage.get).toHaveBeenCalledTimes(1);
-      expect(result).toEqual(prediction);
+    expect(storage.get).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(days);
   })
+
+  it('getDatesWithTraining() - should get new Set if there are not trained words in the storage ', () => {
+    let storage = jasmine.createSpyObj<StorageService>(['get']);
+    let history = new HistoryService(storage);
+
+    storage.get.and.returnValue(null);
+
+    let result = history.getDatesWithTraining();
+
+    expect(storage.get).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(new Set<number>());
+  })
+
+  it('addDatesWithNewWords() - should add date to the storage if a word is trained', function () {
+    let storage = jasmine.createSpyObj<StorageService>(['set', "get"]);
+    let history = new HistoryService(storage);
+
+    let dates = new Set<number>();
+    dates.add(111);
+    dates.add(222);
+    dates.add(333);
+
+    storage.get.and.returnValue(dates);
+
+    history.addDatesWithNewWords(444);
+
+    let expectation = new Set<number>();
+    expectation.add(111);
+    expectation.add(222);
+    expectation.add(333);
+    expectation.add(444);
+
+    expect(storage.set).toHaveBeenCalledWith("startLearning", expectation);
+  });
+
+  it('getDatesWithNweWords() - should get dates for new words', function () {
+    let storage = jasmine.createSpyObj<StorageService>(["get"]);
+    let history = new HistoryService(storage);
+
+    let dates = new Set<number>();
+    dates.add(111);
+    dates.add(222);
+    dates.add(333);
+
+    storage.get.and.returnValue(dates);
+
+    let result = history.getDatesWithNweWords();
+
+    expect(storage.get).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(dates);
+  });
+
+
+  it('getDatesWithNweWords() - should get new Set if there are not dates for new words in the storage', function () {
+    let storage = jasmine.createSpyObj<StorageService>(["get"]);
+    let history = new HistoryService(storage);
+
+    storage.get.and.returnValue(null);
+
+    let result = history.getDatesWithNweWords();
+
+    expect(storage.get).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(new Set<number>());
+  });
+
 });
